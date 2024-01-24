7c1de27b4507f2b2e8abce7969f0db8e0107ee7c
Krichanov Sergey <dsirax@icloud.com>
2024-01-24 09:14:17 +0100

    fix fixtures.

--- homework/conftest.py
+++ homework/conftest.py
@@ -13,7 +13,7 @@ def browser_resolution_mobile(request):
     browser.config.window_width = request.param[0]
     browser.config.window_height = request.param[1]
 
-
+@pytest.fixture(params=[(1920, 1080), (1024, 768), (1680, 1050), (768, 1280), (540, 960), (390, 844)])
 def check_resolution_browser(request):
     resolution = request.param
     resolution_width, resolution_height = resolution
@@ -21,5 +21,5 @@ def check_resolution_browser(request):
     browser.config.window_height = resolution_height
     if resolution == (1920, 1080) or resolution == (1024, 768) or resolution == (1680, 1050):
         return True
-    else:
-        pytest.skip("Skip desktop test")
+    elif resolution == (768, 1280) or resolution == (540, 960) or resolution == (390, 844):
+        return False
--- homework/test_parametrize.py
+++ homework/test_parametrize.py
@@ -3,16 +3,16 @@ from selene import browser, have
 
 
 @pytest.mark.desktop
-@pytest.mark.parametrize('browser_resolution_desktop', [(1920, 1080), (1024, 768), (1680, 1050)], indirect=True)
-def test_github_desktop(browser_resolution_desktop):
+@pytest.mark.parametrize('check_resolution_browser', [(1920, 1080), (1024, 768), (1680, 1050)], indirect=True)
+def test_github_desktop(check_resolution_browser):
     browser.open('https://github.com')
     browser.element(".HeaderMenu-link--sign-in").click()
     browser.element(".auth-form-header").should(have.text("Sign in to GitHub"))
 
 
 @pytest.mark.mobile
-@pytest.mark.parametrize('browser_resolution_mobile', [(768, 1280), (540, 960), (390, 844)], indirect=True)
-def test_github_mobile(browser_resolution_mobile):
+@pytest.mark.parametrize('check_resolution_browser', [(768, 1280), (540, 960), (390, 844)], indirect=True)
+def test_github_mobile(check_resolution_browser):
     browser.open('https://github.com')
     browser.element(".HeaderMenu-toggle-bar").click()
     browser.element(".HeaderMenu-link--sign-in").click()
